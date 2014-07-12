5b98835e5b07efb9ad32b92994a3d775da89b7f6
Aidan Dunn <aidan.chey.d@gmail.com>
2014-07-12 21:29:39 +1200

    Started Gallery
    Test the technology behind the Gallery, and setup the basic
    infurstructer for the gallery. Had a go at setting up Ajax for the
    Larger images, on home server (unsucsesfully).
    Spent a lot of time setting up the examples sites for the gallery to
    link to.

--- pages/gallery.html.php
+++ pages/gallery.html.php
@@ -4,27 +4,18 @@
     <?php include $_SERVER['DOCUMENT_ROOT'].'/includes/nav.php'; ?>
   </nav>
   <div class="header">Portfolio</div>
-  <!--<div class="textCenter" id="categories">
-    <input name="portCats" id="catOne" type="radio" checked />
-    <label for="catOne" class="bar" onclick="category(this)">Vision College - Coding</label>
-    <input name="portCats" id="catTwo" type="radio" />
-    <label for="catTwo" class="bar" onclick="category(this)">Vision College - Design</label>
-     <input name="portCats" id="catThree" type="radio" />
-    <label for="catThree" class="bar" onclick="category(this)">Career</label> 
-  </div>-->
-  <div>
-      <?php 
-        $files = scandir($_SERVER['DOCUMENT_ROOT'].'/images/gallery/');
-        $images = array_diff($files, array('.', '..'));
-        $imgCount = count($images);
-        foreach($images as $key => $value): 
-          $count = 2; ?>
-        
-      <a href="/images/gallery/<?php echo $value; ?>" target="_blank">
-        <img style="background-image:url('/images/gallery/<?php echo $value; ?>'); 
-            width: calc(200%/<?php echo $imgCount; ?>);" 
-          class="slideshow">
-      </a>
-      <?php $count++; endforeach; ?>
-    </div>
+  <p>click the labels to go to the site</p>
+  <section>
+    <?php 
+      $files = scandir($_SERVER['DOCUMENT_ROOT'].'/images/gallery/');
+      $images = array_diff($files, array('.', '..'));
+      print_r($images);
+      foreach($images as $key => $value): ?>
+      <article>
+        <a href="/images/gallery/<?php echo $value; ?>"><img style="background-image:url('/images/gallery/<?php echo $value; ?>'); 
+            width: 200px; height: 200px;"></a>
+        <a href="#placeholder">Site</a>
+      </article>
+    <?php endforeach; ?>
+  </section>
 </div>
