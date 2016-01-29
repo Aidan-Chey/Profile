b9a524c1eba729d383485a709453827abad7fe9d
Aidan Dunn <aidan.chey.d@gmail.com>
2016-01-29 23:30:53 +1300

    Added meta tag "theme-color"

--- website.html.php
+++ website.html.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<title>Profile - Aidan Dunn</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="theme-color" content="#B30000">
 	<meta name="author" content="Aidan Dunn">
 	<meta name="description" content="Introducing Aidan Dunn as a Web Developer/Coder, shows Porfolio / Gallery of work and provides Contact form.">
 	<meta name="keywords" content="Aidan Dunn, Profile, Gallary, Contact, Portfolio, Website, Web Development, Frontend, Hypertext Markup language, HTML5, Javascript, JS, Cascading Style Sheets, CSS, Photoshop, PS, Backend, PHP: Hypertext Preprocessor, Php, Structured Query Language, SQL">
